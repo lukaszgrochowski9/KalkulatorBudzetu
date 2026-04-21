08d02716b526c70bf8d3697a16a7b6b7bb83dd91
lgrochowski <lgrochowski20@gmail.com>
2026-04-21 18:17:07 +0200

    dodanie funkcji czyszczacej przykladowe dane html i na nowo generuje kafelki transakcji

--- js/script.js
+++ js/script.js
@@ -12,6 +12,41 @@ const transactionsList = document.querySelector('.transactions-list');
 
 let transactions = [];
 
+const categoryMap = {
+    food: "Jedzenie",
+    transport: "Transport",
+    housing: "Mieszkanie",
+    entertainment: "Rozrywka",
+    salary: "Wynagrodzenie",
+    other: "Inne"
+};
+
+function updateUI() {
+    transactionsList.innerHTML = '';
+
+    transactions.forEach(transaction => {
+        const sign = transaction.type === 'expense' ? '-' : '+';
+        const item = document.createElement('div');
+        item.classList.add('transaction-item', transaction.type);
+
+        const dateObj = new Date(transaction.date);
+        const dateStr = new Intl.DateTimeFormat('pl-PL', { day: 'numeric', month: 'long', year: 'numeric' }).format(dateObj);
+        const amountStr = new Intl.NumberFormat('pl-PL', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(transaction.amount) + ' PLN';
+
+        item.innerHTML = `
+            <div class="transaction-info">
+                <h4>${transaction.name}</h4>
+                <span>${categoryMap[transaction.category]} • ${dateStr}</span>
+            </div>
+            <div class="transaction-amount">
+                ${sign}${amountStr}
+            </div>
+        `;
+
+        transactionsList.appendChild(item);
+    });
+}
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -26,4 +61,7 @@ form.addEventListener('submit', function(e) {
 
     transactions.push(transaction);
     form.reset();
+    updateUI();
 });
+
+updateUI();
