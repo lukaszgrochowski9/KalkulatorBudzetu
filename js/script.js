4039213a50148ed6704b86337671c70885a568cb
lgrochowski <lgrochowski20@gmail.com>
2026-04-21 18:05:43 +0200

    przypisanie do zmiennych najwazniejszych elementow z index.php

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('.transaction-form');
+const typeInput = document.querySelector('#type');
+const nameInput = document.querySelector('#name');
+const amountInput = document.querySelector('#amount');
+const categoryInput = document.querySelector('#category');
+const dateInput = document.querySelector('#date');
+
+const balanceDisplay = document.querySelector('.balance .amount');
+const incomesDisplay = document.querySelector('.incomes .amount');
+const expensesDisplay = document.querySelector('.expenses .amount');
+const transactionsList = document.querySelector('.transactions-list');
